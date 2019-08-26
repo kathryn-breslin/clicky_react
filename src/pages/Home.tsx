3f60f7760d45981f1b179cd91f8accc926d8daef
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-26 17:45:49 -0400

    click handler

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,50 @@ import React, { Component } from "react";
 import { Card } from "../components";
 import seaCreatures from "../seaCreatures.json";
 
+interface ICreatures{
+    id: number;
+    name: string;
+    image: string;
+    // counter: number, 
+    // clicked: string
+}
+
+interface IState {
+    seaCreatures: ICreatures[], 
+    id: number, 
+    name: string, 
+    image: string;
+    counter: number,
+    clicked: string[]
+}
+
 class Home extends Component {
-  state = {
-    seaCreatures, 
+  state: IState = {
+    seaCreatures: [], 
+    id: 0, 
+    name: "", 
+    image: "",
     counter: 0, 
     clicked: []
   };
 
-  shuffle = (id: any) => {
-    const { seaCreatures, counter } = this.state;
-    seaCreatures.sort(() => Math.random() - 0.5);
+//   shuffle = (id: any) => {
+//     const { seaCreatures, counter } = this.state;
+//     seaCreatures.sort(() => Math.random() - 0.5);
 
-    this.setState({ counter: counter + 1})
+//     this.setState({ counter: counter + 1})
+//   }
+
+  handleClick = (id: any) => {
+      const { clicked, counter } = this.state;
+      if (clicked.indexOf(id) === -1) {
+          this.setState({ clicked: clicked.concat(id), counter: counter + 1})
+      }
   }
 
 
   render() {
-    const { seaCreatures } = this.state;
+
     return (
       <div>
           <div className="container">
@@ -32,7 +59,8 @@ class Home extends Component {
                 key={creature.id}
                 image={creature.image}
                 name={creature.name}
-                shuffle={this.shuffle}
+                // shuffle={this.shuffle}
+                handleClick={this.handleClick}
               />
             </div>
           </div>
