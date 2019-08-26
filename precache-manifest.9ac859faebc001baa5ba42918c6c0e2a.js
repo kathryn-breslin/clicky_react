adad10a65a1a38bc2eaea267703f36eecbaa65fc
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-26 11:30:25 -0400

    Updates

---
+++ precache-manifest.9ac859faebc001baa5ba42918c6c0e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf59e6a69421b752d7658e3c8306e23",
+    "url": "/kathryn-breslin/clicky_react.git/index.html"
+  },
+  {
+    "revision": "0dffbcf072b9f6bfc172",
+    "url": "/kathryn-breslin/clicky_react.git/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "7cae3eaa017a1c9eb6e6",
+    "url": "/kathryn-breslin/clicky_react.git/static/js/2.7a12dfc3.chunk.js"
+  },
+  {
+    "revision": "0dffbcf072b9f6bfc172",
+    "url": "/kathryn-breslin/clicky_react.git/static/js/main.02fc5345.chunk.js"
+  },
+  {
+    "revision": "4ed552f71ef2b4bc68dc",
+    "url": "/kathryn-breslin/clicky_react.git/static/js/runtime~main.b1cd3ad9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kathryn-breslin/clicky_react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
