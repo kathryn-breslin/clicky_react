e49719aa55b4e945bcc5017ba388a5384436820e
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-26 11:58:36 -0400

    new structure

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card ({/* props */}) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                {/* Pass and add props */}
+            </div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Card } from "../components";
+
+class Home extends Component {
+
+    render() {
+        return (
+            <Card/>
+        )
+    }
+}
+
+export default Home;
