ca9e450633fe205db384ffb47d3f4e8940c80d69
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-26 16:15:03 -0400

    new image render

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Card.css";
 
-function Card (props: { name: string | undefined; id: number; image: string | undefined; }) {
-    return (
-        <div className="card">
-            <div className="img-container">
-                <img alt={props.name} src={props.image}></img>
-            </div>
-        </div>
-    )
+function Card(props: {
+  name: string | undefined;
+  id: number;
+  image: string | undefined;
+}) {
+  return (
+    <div className="card">
+      <img alt={props.name} src={props.image}></img>
+    </div>
+  );
 }
 
 export default Card;
