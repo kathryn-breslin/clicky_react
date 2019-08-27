a93fc5504ef51af5f74ab2a1aa3d7ef2b64cc15e
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-27 10:14:28 -0400

    new interface for json object

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,16 +5,13 @@ interface IProps {
     name: string;
     id: any;
     image: string;
-    handleClick: (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
+    shuffle: (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
 }
-
-const Card = ({ name, id, image, handleClick }: IProps) => {
+const Card = ({ name, id, image, shuffle }: IProps) => {
   return (
     <div className="card">
-      <img 
-      alt={name} 
-      src={image}
-      onClick={() => handleClick(id)} className="shuffle"
+      <img alt={name} src={image}
+      onClick={() => shuffle(id)} className="shuffle"
       ></img>
 
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,55 +2,65 @@ import React, { Component } from "react";
 import { Card } from "../components";
 import seaCreatures from "../seaCreatures.json";
 
-interface ICreatures{
-    id: number;
-    name: string;
-    image: string;
-    // counter: number, 
-    // clicked: string
+interface ICreatures {
+  id: any;
+  name: string;
+  image: string;
 }
 
 interface IState {
-    seaCreatures: ICreatures[], 
-    id: number, 
-    name: string, 
-    image: string;
-    counter: number,
-    clicked: string[]
+  seaCreatures: ICreatures[];
+  counter: number;
+  clicked: string[];
 }
 
 class Home extends Component {
   state: IState = {
-    seaCreatures: [], 
-    id: 0, 
-    name: "", 
-    image: "",
-    counter: 0, 
+    seaCreatures,
+    counter: 0,
     clicked: []
   };
 
-//   shuffle = (id: any) => {
-//     const { seaCreatures, counter } = this.state;
-//     seaCreatures.sort(() => Math.random() - 0.5);
+  shuffle = (id: any) => {
+    const { seaCreatures, counter } = this.state;
+    seaCreatures.sort(() => Math.random() - 0.5);
 
-//     this.setState({ counter: counter + 1})
-//   }
+    console.log("ID: " + id);
+
+    this.setState({ counter: counter + 1 });
+  };
 
   handleClick = (id: any) => {
-      const { clicked, counter } = this.state;
-      if (clicked.indexOf(id) === -1) {
-          this.setState({ clicked: clicked.concat(id), counter: counter + 1})
-      }
-  }
+    const { clicked } = this.state;
+    if (clicked.indexOf(id)) {
+      this.setState({ clicked: clicked.concat(id) });
+    } else {
+      console.log("redo");
+    }
+  };
 
+  //   scoring = () => {
+  //       const { counter, totalScore, id } = this.state;
+  //       const score = counter + 1;
+
+  //       this.setState({ totalScore: score });
+
+  //       if (score >= totalScore){
+  //         console.log(id)
+  //         this.setState({totalScore: score})
+  //       }
+  //       else if (score === 12) {
+  //           console.log("You win!")
+  //       }
+  //       this.shuffle(id);
+  //   }
 
   render() {
-
     return (
       <div>
-          <div className="container">
-              <h1>{this.state.counter}</h1>
-          </div>
+        <div className="container">
+          <h1>{this.state.counter}</h1>
+        </div>
         {seaCreatures.map(creature => (
           <div className="row">
             <div className="col-12">
@@ -59,8 +69,8 @@ class Home extends Component {
                 key={creature.id}
                 image={creature.image}
                 name={creature.name}
-                // shuffle={this.shuffle}
-                handleClick={this.handleClick}
+                shuffle={this.shuffle}
+                // handleClick={this.handleClick}
               />
             </div>
           </div>
