1bb0ecbee5b330d21bb9b0b7d90a7138bc277595
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-28 10:40:05 -0400

    Updates

---
+++ precache-manifest.3bb7c671e4444d5107bab672a988daef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb4736d0912bebd49eeee1f3526bd97",
+    "url": "/clicky_react/index.html"
+  },
+  {
+    "revision": "1d0c9d7820961537f7b1",
+    "url": "/clicky_react/static/css/main.09559e9c.chunk.css"
+  },
+  {
+    "revision": "f5cc78477bfb8ede80c6",
+    "url": "/clicky_react/static/js/2.054d9823.chunk.js"
+  },
+  {
+    "revision": "1d0c9d7820961537f7b1",
+    "url": "/clicky_react/static/js/main.df757bf3.chunk.js"
+  },
+  {
+    "revision": "043939870a21e986cc64",
+    "url": "/clicky_react/static/js/runtime~main.da9d716c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kathryn-breslin/clicky_react.git/precache-manifest.9ac859faebc001baa5ba42918c6c0e2a.js"
+  "/clicky_react/precache-manifest.3bb7c671e4444d5107bab672a988daef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kathryn-breslin/clicky_react.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
